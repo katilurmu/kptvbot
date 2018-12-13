a6274965b1c0540e9b92e52a8eea09176be7af62
katilurmu <45826703+katilurmu@users.noreply.github.com>
2018-12-13 17:22:04 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,7 +10,7 @@ exports.run = (client, message, params) => {
   .setColor("RANDOM")
   .addField("**» Botun Ana Komutları**", `a!yardım = BOT Komutlarını Atar. \na!bilgi = BOT Kendisi Hakkında Bilgi Verir. \na!ping = BOT Gecikme Süresini Söyler. \na!davet = BOT Davet Linkini Atar. \na!site  = BOT Site Linki Atar. \na!sunucutanıt = Sunucunuzu tanıtır. \na!destekgrup = Botun destek sunucusunun davet linkini atar.`)
   .addField("**» Eğlence Komutları**",`a!gif = Gifleri gösterir.`)
-  .setFooter('Alpha Bot Güncel Sürüm [ BETA v0.2.5 ]')
+  .setFooter('KP-TV Bot Güncel Sürüm [ BETA v0.2.5 ]')
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
@@ -34,5 +34,5 @@ exports.conf = {
 exports.help = {
   name: 'yardım',
   description: 'Tüm komutları gösterir.',
-  usage: 'a!yardım [komut]'
+  usage: 'k!yardım [komut]'
 };
